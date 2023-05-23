06778e0ad58bdb440b1481c38f329d7f6960edff
Alex <topmlgfordota1@gmail.com>
2023-05-23 21:03:59 +0300

    page routing reworked

--- client/pages/candidate-application.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex items-center h-screen bg-purple">
-    <!--        <CandidateApplication />-->
-  </div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
