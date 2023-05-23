a0ce3295504fafaf28d05b5f9a972f20aa23920a
Alexey <llisicyn@bk.ru>
2023-05-23 21:16:14 +0300

    fix: candidates

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -7,10 +7,10 @@
     <!--    <button @click="userStore.user.role = Role.FRAME">Кадр</button>-->
   </div>
 
-  <Candidate v-if="userStore.user?.role === Role.CANDIDATE" />
+  <!-- <Candidate v-if="userStore.user?.role === Role.CANDIDATE" />
   <Curator v-if="userStore.user?.role === Role.CURATOR" />
-  <!--  <Interns v-if="userStore.user.role === Role.TRAINEE" />-->
-  <Frame v-if="userStore.user?.role == Role.FRAME" />
+    <Interns v-if="userStore.user.role === Role.TRAINEE" />-->
+  <Frame v-if="userStore.user?.role == Role.FRAME" />`` -->
 </template>
 
 <script setup lang="ts">
