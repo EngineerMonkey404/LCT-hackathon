4d196f939333fe5e4b67264881b289df48b91a64
Alex <topmlgfordota1@gmail.com>
2023-05-24 22:13:12 +0300

    invites backend rdy

--- server/src/invites/invites.controller.ts
+++ server/src/invites/invites.controller.ts
@@ -1,8 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Query } from '@nestjs/common';
+import { Role } from '../auth/models/role.enum';
+import { InvitesService } from './invites.service';
 
 @Controller('invites')
 export class InvitesController {
+  constructor(private readonly invitesService: InvitesService) {}
+  @Get('generate')
+  async generateInvite(@Query('role') role: Role) {
+    return await this.invitesService.generateInvite(role);
+  }
 
-    @Get('generate')
-    async generateInvite()
+  @Delete(':path')
+  async deleteInvite(@Param('path') path: string) {
+    await this.invitesService.deleteInvite(path);
+  }
 }
--- server/src/invites/invites.service.ts
+++ server/src/invites/invites.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Invites } from './invites.model';
+import { v4 as uuid } from 'uuid';
+import { Role } from '../auth/models/role.enum';
 
 @Injectable()
-export class InvitesService {}
+export class InvitesService {
+  constructor(@InjectModel(Invites) private invitesModel: typeof Invites) {}
+  async generateInvite(role: Role) {
+    const random = uuid();
+    await this.invitesModel.create({ path: random, role: role });
+    return random;
+  }
+
+  async deleteInvite(path: string) {
+    await this.invitesModel.destroy({ where: { path: path } });
+  }
+}
