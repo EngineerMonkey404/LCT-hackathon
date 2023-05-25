ed716e0c9c8b00e79b094eafd679a2f380fc2c03
Alexey <llisicyn@bk.ru>
2023-05-25 23:43:31 +0300

    feat: test trainee page

---
+++ client/pages/trainee/solve-test/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto">
+        <SolveTest />
+    </div>
+</template>
+
+<script setup lang="ts">
+import SolveTest from '~/components/trainee/SolveTest.vue';
+//из роут парам достаем id заявки и получаем тест по нему
+// и пушим или id или application  в компоненте
+</script>
+
+<style scoped>
+</style>
